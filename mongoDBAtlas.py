2722af76dd3644c91edb078274edaee284a95a53
Jonathan Wong <jrwongacademy@gmail.com>
2019-10-19 13:20:56 -0400

    adds to database

--- mongoDBAtlas.py
+++ mongoDBAtlas.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 import pprint
+import csv
 
-
+'''
 id = "10006546"
 
 #print('hello world')
@@ -16,4 +17,21 @@ airbnb = client.get_database("sample_airbnb")
 airbnb_records = airbnb.listingsAndReviews
 
 num_docs = airbnb_records.count_documents({})
-print(num_docs)
+pprint.pprint(airbnb_records.find_one({"_id": id}))
+'''
+
+client = MongoClient("mongodb://Sai:saisrinivas1@cluster0-shard-00-00-vaulk.mongodb.net:27017,cluster0-shard-00-01-vaulk.mongodb.net:27017,cluster0-shard-00-02-vaulk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+colgate_db = client.get_database("colgate_database")
+pricing = colgate_db.pricing
+
+with open('hack_ru.csv', newline='') as csvfile:
+    spam_reader = csv.reader(csvfile, delimiter=' ',quotechar='|')
+    reader = csv.DictReader(csvfile)
+    counter = 0
+    for row in reader:
+        #ignore first row with the titles
+        dict = row
+        pricing.insert_one(dict).inserted_id
+        break
+
+print("finished")
