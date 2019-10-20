1a8dc0f82b993f01a8253126500339f7cc556803
anthonysiu2000 <48339055+anthonysiu2000@users.noreply.github.com>
2019-10-20 05:37:17 -0400

    Add files via upload

--- Colgate Project.R
+++ Colgate Project.R
@@ -1,11 +1,10 @@
 install.packages("rmarkdown")
 library(rmarkdown)
 
-ColgateData <- read.csv(file="C:/Users/tonys/OneDrive/Documents/R/hack_ru.csv")
+ColgateData <- read.csv(file="C:/Users/tonys/OneDrive/Documents/R/hack_ru2(1).csv")
 
 ## Removes the columns "ingredients" and "x"
 TempData <- subset(ColgateData, select = -X)
-TempData <- subset(TempData, select = -ingredients)
 ## Linear model to get the weights
 Model <- lm(price_per_100g_ml_dollars ~ ., data = TempData)
 ModelSummary <- summary(Model)
@@ -17,4 +16,4 @@ numCompanies <- length(unique(ColgateData$company))
 numIngredients <- length(TempData)-5
   
 ##writes the coefficient data into 
-write.csv(ModelCoeffs, file = "Coeff.csv")
+write.csv(ModelCoeffs, file = "Coeff4.csv")
